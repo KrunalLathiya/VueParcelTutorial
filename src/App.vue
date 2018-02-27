a3cc9ad0eb8cb976258f706de80cf532a500aa25
KrunalLathiya <krunallathiya21@gmail.com>
2018-02-28 02:28:54 +0530

    Set up Vue js With Parcel module bundler

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h3>{{ data }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      data: 'Welcome to parcel bundler'
+    }
+  }
+}
+</script>
